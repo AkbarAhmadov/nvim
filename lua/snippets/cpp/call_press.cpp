2e1a0e8a6c778eb1eebae70b9b72271be61d9d6f
duel2026 <gpt71253@gmail.com>
2026-03-29 00:01:48 +0400

    Adding compress

--- lua/snippets/cpp/call_press.cpp
+++ lua/snippets/cpp/call_press.cpp
@@ -1,3 +1,15 @@
-int compress(vector <int> &v) {
-    
+int compress(vector <int> &v, int s = 1) {
+    if (v.empty()) {
+        return 0;
+    }
+    vector <int> v1 = v;
+    sort(all(v1));
+    v1.erase(unique(all(v1)), v1.end());
+    int mx = s;
+    for (int &i : v) {
+        int k = lower_bound(all(v1), i) - v1.begin() + s;
+        i = k;
+        mx = max(mx, k);
+    }
+    return mx;
 }
